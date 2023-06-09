cb3bd7e16a296919df451fa868d63502f1325c4e
SANJIB KUMAR DAS <sanjibdas.23012002@gmail.com>
2023-06-09 21:55:03 +0530

    assignment

--- prog24.py
+++ prog24.py
@@ -1,7 +1,6 @@
 # Adding certain numbers to their reversals sometimes produces a palindromic number. For instance, 241 + 142 = 383. Sometimes, we have to repeat the process. For instance, 84 + 48 =132 and 132 + 231 = 363. Write a program that finds both two-digit numbers for which this process must be repeated more than 20 times to obtain a palindromic number
 
 def main():
-    print("This program finds both two-digit numbers for which this process must be repeated more than 20 times to obtain a palindromic number")
     for i in range(10,100):
         number = i
         for j in range(20):
@@ -10,6 +9,6 @@ def main():
                 break
         if j == 19:
             print(i)
-    print("Found on iteration no: ",number)
+    print("Palindromic no is: ",number)
 if __name__ == "__main__":
     main()
